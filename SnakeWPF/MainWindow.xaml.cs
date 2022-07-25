f3549165569f30719f4b31eab79cca66a12a8a0e
Ahmad <ahmadhalabieh16@gmail.com>
2022-07-25 19:50:40 +0300

    Import usings

--- SnakeWPF/MainWindow.xaml.cs
+++ SnakeWPF/MainWindow.xaml.cs
@@ -1,4 +1,16 @@
-﻿using Ellipse = System.Windows.Shapes.Ellipse;
+﻿using SnakeWPF.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+using Ellipse = System.Windows.Shapes.Ellipse;
 using Rectangle = System.Windows.Shapes.Rectangle;
 
 namespace SnakeWPF
